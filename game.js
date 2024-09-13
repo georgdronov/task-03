6b7bba71f14639b7653ba06ca05012fda59b2f5a
Georg Dronov <dronovgeorg72@gmail.com>
2024-09-13 09:53:55 +0300

    add game logic, need tests

--- game.js
+++ game.js
@@ -1,9 +1,4 @@
-`What i need
-
-export
-error log + msg
-
-game logic 
+`
 
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠞⠋⠈⣷⠶⠶⠶⠶⣤⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -29,10 +24,54 @@ game logic
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠿⢷⣶⣤⣤⣤⣤⣴⣶⣾⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+`;
 
-maybe use like a preview
+import readlineSync from "readline-sync";
+import { generateKey } from "./keyGenerator.js";
+import { generateHmac } from "./hmacGenerator.js";
+import { determineWinner } from "./rules.js";
+import { generateHelpTable } from "./helpTable.js";
 
+const args = process.argv.slice(2);
+if (args.length < 3 || args.length % 2 === 0) {
+  console.log(
+    "Error: You must provide an odd number of moves (3 or more). Example: node game.js rock paper scissors"
+  );
+  process.exit(1);
+}
 
-add link to prove HMAC (optional)
+const moves = args;
 
-`
+const computerMoveIndex = Math.floor(Math.random() * moves.length);
+const computerMove = moves[computerMoveIndex];
+const key = generateKey();
+const hmac = generateHmac(key, computerMove);
+
+console.log(`HMAC: ${hmac}`);
+
+let userMoveIndex = -1;
+while (userMoveIndex < 0 || userMoveIndex < moves.length) {
+  console.log("Available moves:");
+  moves.forEach((move, index) => {
+    console.log(`${index + 1}. ${move}`);
+  });
+  console.log("0 - Exit");
+  console.log("? - Help");
+
+  const userInput = readlineSync.question("Enter your move: ");
+
+  if (userInput === "?") {
+    generateHelpTable(moves);
+  } else if (userInput === "0") {
+    console.log("FoodBye! Have a nice day!");
+    process.exit(0);
+  } else {
+    userMoveIndex = parseInt(userInput) - 1;
+  }
+}
+
+const userMove = moves[userMoveIndex];
+console.log(`Your move: ${userMove}`);
+console.log(`Computer move: ${computerMove}`);
+console.log(determineWinner(userMove, computerMove, moves));
+console.log(`HMAC key: ${key}`);
